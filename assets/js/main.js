621d323e4ea2fb5cf6c3f8f9c5ff00ff7e74e6de
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-21 18:14:42 +0200

    badly done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,59 @@
 //*********************************************** */
+//***************** EXERCISE ******************** */
+//*********************************************** */
+
+let numberBox = document.getElementById("Container");
+numberBox.classList.add('d-flex')
+numberBox.classList.add('flex-wrap')
+
+//let singleBox = document.createElement("div");
+
+//numberBox.classList.add("neon-button")
+
+//numberBox.append(singleBox);
+
+
+
+for (let i = 1; i <=100; i++) {
+
+    let box = document.createElement("div");
+    box.classList.add('col-1')
+   
+    if (i % 3 == 0 && i % 5 == 0){
+        
+        console.log(`FIZZBUZZ`);
+        
+        box.classList.add('neon-button');
+        box.innerText = `${i} FIZZBUZZ`;
+
+    }else if(i % 3 == 0){
+        
+        console.log(`FIZZ`);
+        
+        box.classList.add('bg-primary');
+        box.innerText = `${i} FIZZ`;
+
+    
+    }else if (i % 5 == 0){
+        
+        console.log(`BUZZ`);
+        box.classList.add('bg-secondary');
+        box.innerText =`${i} BUZZ`;
+
+    }else{
+        
+        console.log(`${i}`);
+        box.classList.add('bg-light');
+        box.innerText =`${i}`;
+
+    }
+
+    numberBox.append(box);
+} 
+
+
+
+//*********************************************** */
 // ****************** cicle's check **************
 //*********************************************** */
 
@@ -28,12 +83,12 @@
 //********** createElement's check ************ */
 //********************************************* */
 
-let container = document.getElementById('gettable');
-let singleBox = document.createElement('div');
-// add a .class
-singleBox.classList.add('neon-button');
-// add some text into the <tag>
-singleBox.innerText = 'ciao';
-console.log(singleBox)
+// let container = document.getElementById('gettable');
+// let singleBox = document.createElement('div');
+// // add a .class
+// singleBox.classList.add('neon-button');
+// // add some text into the <tag>
+// singleBox.innerText = 'ciao';
+// console.log(singleBox)
 
-container.append(singleBox);
+// container.append(singleBox);
