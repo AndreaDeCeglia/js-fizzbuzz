470d2c792c3c710236435321d8654d95b2311f09
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-21 15:11:33 +0200

    innerHTML's check done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
+//*********************** */
 // *** cicle's check ****
+//*********************** */
 
-for( let i = 1; i <= 100; i++){
+// for( let i = 1; i <= 100; i++){
+//     console.log(i)
+//     document.getElementById('gettable').innerHTML = `${i}`
+// }
+
+// *********************
+// *** innerHTML's check ***
+//********************** */
+
+let numberBox = document.getElementById('gettable');
+
+for ( let i = 1; i <= 100; i++ ){
     console.log(i)
-    document.getElementById('gettable').innerHTML = `${i}`
-}
 
+    let string = `<p>il tuo numero è : ${i} !!</p>`
+    numberBox.innerHTML += string;
+}
