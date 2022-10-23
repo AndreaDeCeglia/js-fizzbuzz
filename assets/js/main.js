636ac30b852aea5f3761a157716bc19799cd1270
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-23 17:15:24 +0200

    done it better

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,55 +2,90 @@
 //***************** EXERCISE ******************** */
 //*********************************************** */
 
-let numberBox = document.getElementById("Container");
-numberBox.classList.add('d-flex')
-numberBox.classList.add('flex-wrap')
+let numberBox = document.getElementById("fizzbuzzcontainer");
+numberBox.classList.add('d-flex');
+numberBox.classList.add('flex-wrap');
 
-//let singleBox = document.createElement("div");
+let from;
+let till;
+const findOut = document.getElementById('findOutBtn');
+const reset = document.getElementById('resetBtn');
+findOut.addEventListener('click', start);
+findOut.addEventListener('click', disableButton);
+reset.addEventListener('click', clearBox);
+reset.addEventListener('click', enableButton);
 
-//numberBox.classList.add("neon-button")
+function start(){
+    from = parseInt(document.getElementById('from').value);
+    till = parseInt(document.getElementById('till').value);
 
-//numberBox.append(singleBox);
+    if (from > till){
+        alert("You're going against the Math's rules man!!!!!!!")
+    } else {
 
+        for (let i = from; i <=till; i++) {
 
-
-for (let i = 1; i <=100; i++) {
-
-    let box = document.createElement("div");
-    box.classList.add('col-1')
-   
-    if (i % 3 == 0 && i % 5 == 0){
+            let box = document.createElement("div");
+            box.classList.add('col-1')
+            box.classList.add('m-1');
+            box.classList.add('text-center');
+           
+            if (i % 3 == 0 && i % 5 == 0){
+                
+                console.log(`FIZZBUZZ`);
+                
+                box.classList.add('neon-cell');
+                box.innerText = `FIZZBUZZ`;
         
-        console.log(`FIZZBUZZ`);
+            }else if(i % 3 == 0){
+                
+                console.log(`FIZZ`);
+                
+                box.classList.add('bg-primary');
+                box.innerText = `FIZZ`;
         
-        box.classList.add('neon-button');
-        box.innerText = `${i} FIZZBUZZ`;
-
-    }else if(i % 3 == 0){
+            
+            }else if (i % 5 == 0){
+                
+                console.log(`BUZZ`);
+                box.classList.add('bg-secondary');
+                box.innerText =`BUZZ`;
         
-        console.log(`FIZZ`);
+            }else{
+                
+                console.log(`${i}`);
+                box.classList.add('bg-light');
+                box.classList.add('text-dark');
+                box.innerText =`${i}`;
         
-        box.classList.add('bg-primary');
-        box.innerText = `${i} FIZZ`;
-
-    
-    }else if (i % 5 == 0){
+            }
         
-        console.log(`BUZZ`);
-        box.classList.add('bg-secondary');
-        box.innerText =`${i} BUZZ`;
-
-    }else{
-        
-        console.log(`${i}`);
-        box.classList.add('bg-light');
-        box.innerText =`${i}`;
+            numberBox.append(box);
+        }     
 
     }
+    
+}
 
-    numberBox.append(box);
-} 
 
+function clearBox(){
+    document.getElementById('fizzbuzzcontainer').innerHTML = "";
+    document.getElementById("from").value = "";
+    document.getElementById("till").value = "";
+    
+}
+
+function disableButton() {
+    document.getElementById("findOutBtn").disabled = true;
+    document.getElementById("resetBtn").disabled = false;
+}
+
+function enableButton() {
+    document.getElementById("findOutBtn").disabled = false;
+    document.getElementById("resetBtn").disabled = true;
+    
+}
+resetBtn.disabled = true;
 
 
 //*********************************************** */
